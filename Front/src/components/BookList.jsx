fdcdf4991d78b156ab076bd09642446096d93d3c
jmcaussade <jmcaussade@miuandes.cl>
2024-08-08 01:12:08 -0400

    Nombre autor agragado con exito a lista de libros

--- Front/src/components/BookList.jsx
+++ Front/src/components/BookList.jsx
@@ -4,12 +4,14 @@ import BookForm from './BookForm';
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
+  const [authors, setAuthors] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [editingBook, setEditingBook] = useState(null);
 
   useEffect(() => {
     fetchBooks();
+    fetchAuthors();
   }, []);
 
   const fetchBooks = async () => {
@@ -20,6 +22,21 @@ const BookList = () => {
     } catch (error) {
       setError(error.message);
       setLoading(false);
+    }
+  };
+
+  const fetchAuthors = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/authors');
+      const authorsData = response.data;
+      // Create a mapping from author ID to author name
+      const authorsMap = authorsData.reduce((acc, author) => {
+        acc[author._id] = author.name;
+        return acc;
+      }, {});
+      setAuthors(authorsMap);
+    } catch (error) {
+      setError(error.message);
     }
   };
 
@@ -49,14 +66,15 @@ const BookList = () => {
       <BookForm fetchBooks={fetchBooks} editingBook={editingBook} clearEditing={clearEditing} />
       <ul>
         {books.map((book) => (
-          <li key={book.id}>
+          <li key={book._id}>
             <h2>{book.name}</h2>
             <p>Summary: {book.summary}</p>
             <p>Date of Publication: {book.date_of_publication}</p>
             <p>Number of Sales: {book.number_of_sales}</p>
+            <p>Author Name: {authors[book.author] || 'Unknown'}</p>
             <p>Author ID: {book.author}</p>
             <button onClick={() => editBook(book)}>Edit</button>
-            <button onClick={() => deleteBook(book.id)}>Delete</button>
+            <button onClick={() => deleteBook(book._id)}>Delete</button>
           </li>
         ))}
       </ul>
